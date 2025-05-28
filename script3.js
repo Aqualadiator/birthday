bfc094a046f422c5d348f13a32384a071385aca1
Aqualadiator <yisagi376@gmail.com>
2025-05-28 09:19:48 +0800

    Add files via upload

--- script3.js
+++ script3.js
@@ -5,7 +5,25 @@ document.getElementById("celebrateBtn").addEventListener("click", () => {
     origin: { y: 0.6 }
   });
 
-  // Optional audio
-  const audio = new Audio("https://www.youtube.com/watch?v=TjEbuWP3hjg");
+  
+
+  const audio = new Audio("hbd.mp3");
   audio.play();
+
+// Falling hearts
+for (let i = 0; i < 30; i++) {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDelay = Math.random() * 3 + "s";
+  heart.style.animationDuration = 4 + Math.random() * 3 + "s";
+  document.getElementById("heartContainer").appendChild(heart);
+
+  // Remove after animation
+  setTimeout(() => {
+    heart.remove();
+  }, 8000);
+}
+
+
 });
